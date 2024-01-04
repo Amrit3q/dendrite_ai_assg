dc27b19f98855f63f5822d00dccac7fcd304c637
Amrit <amritanshu030801@gmail.com>
2024-01-04 21:52:39 +0530

    first

---
+++ src/Image.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import axios from 'axios'
+import { useState } from 'react'
+const Image = () => {
+    
+    return (
+    <div>
+        <input type="file" />
+        <button>Upload</button>
+    </div>
+  )
+}
+
+export default Image
---
+++ src/sample_home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Image from './Image'
+const sample_home = () => {
+  return (
+    <div>
+       <h1>Image upload app</h1> 
+       submit here the sample:
+        <Image />
+    </div>
+    
+  )
+}
+
+export default sample_home
